6a5d18f9e9262eaa0441f203f3c5608e0ceb4ffb
RSg-glitch <rajaandanshetti@gmail.com>
2026-01-08 14:58:06 +0530

    save

---
+++ bank.py
@@ -0,0 +1,7 @@
+def bank_details(account_number, holder_name, account_type):
+    result = {
+        "Account Number": account_number,
+        "Holder Name": holder_name,
+        "Account Type": account_type
+    }
+    return result
