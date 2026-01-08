ece736740da2ba3f56e6279542ef646f9770a16e
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2026-01-08 16:48:12 +0530

    save

--- bank.py
+++ bank.py
@@ -5,3 +5,9 @@ def bank_details(account_number, holder_name, account_type):
         "Account Type": account_type
     }
     return result
+
+if __name__ == "__main__":
+    account_number = "123456789"
+    holder_name = "John Doe"
+    account_type = "Savings"
+    print(bank_details(account_number, holder_name, account_type))
